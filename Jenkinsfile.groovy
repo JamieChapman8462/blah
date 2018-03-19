bae43771c40979271e72d62e970996a0430ce2ed
jchapman <jamiechapman8462@gmail.com>
2018-03-19 14:01:30 +0000

    test 3

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,29 @@
+pipeline {
+  agent any
+  properties([pipelineTriggers([[$class: 'GitHubPRTrigger', events: [[$class: 'GitHubPROpenEvent']], spec: '* * * * *', triggerMode: 'CRON']])])
+  stages {
+    stage('checkout repository') {
+      steps {
+        checkout([$class: 'GitSCM', branches: [[name: 'refs/heads/develop']],
+          doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs:
+          [[credentialsId: '3535a10f-0df0-4f9d-bff7-c50bef95d944', url: 'git@github.com:JamieChapman8462/blah.git']]])
+      }
+    }
+    stage('Build') {
+      steps {
+        sh 'echo Hello world'
+      }
+      post {
+        always {
+          publishHTML([allowMissing: true,
+          alwaysLinkToLastBuild: true,
+          keepAll: true,
+          reportDir: 'vapi-service/build/reports/tests/test',
+          reportFiles: 'index.html',
+          reportName: 'Unit test report',
+          reportTitles: ''])
+        }
+      }
+    }
+  }
+}
